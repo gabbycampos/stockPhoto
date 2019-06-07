e028252e6d0c173a68f9ceedf585e37e7dd825bd
Gabby Campos <gabbycampos@Gabbys-MacBook-Pro.local>
2019-06-06 19:41:03 -0500

    Updates

---
+++ precache-manifest.eedbfda0b2e77467a7840eaef8e41201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50c6864f1e1047b5a7eb",
+    "url": "/stockPhoto/static/js/runtime~main.efcf9c52.js"
+  },
+  {
+    "revision": "d395f67325d141519a59",
+    "url": "/stockPhoto/static/js/main.9757fab2.chunk.js"
+  },
+  {
+    "revision": "4bfbb378bccb0a9cae8d",
+    "url": "/stockPhoto/static/js/2.63b07aa4.chunk.js"
+  },
+  {
+    "revision": "d395f67325d141519a59",
+    "url": "/stockPhoto/static/css/main.8c0ded82.chunk.css"
+  },
+  {
+    "revision": "c4ebc9c552f7376de50915616a3616d7",
+    "url": "/stockPhoto/index.html"
+  }
+];
